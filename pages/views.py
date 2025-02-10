c4f1a2469e145287db06770410156a113768dddb
Ftnn <fufi.fr@yandex.ru>
2025-02-10 13:51:56 +0400

    test

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def events(request):
+    return render(request, 'pages/events.html')
